81572ee6867ba2da6209febc026816fa3d13e130
katsew <yu@yk-no-iMac.local>
2014-11-24 00:27:28 +0900

    initial commit
    introducing Test Runner 'karma' and Test Framework 'jasmin2.0'

---
+++ test/test001.spec.js
@@ -0,0 +1,17 @@
+describe ('test001', function (){
+  it ('function add', function (){
+    expect (plus (1, 1)).toBe (2);
+  });
+
+  it ('function minus', function (){
+    expect (minus (4, 3)).toBe (1);
+  });
+
+  it ('undef', function (){
+    expect (undef()).toBeDefined();
+  });
+
+  it ('not undef', function (){
+    expect (notUndef()).toBeDefined();
+  });
+});
